cebc69a48cf30596866a4f12979f4d75db6d9162
ShadowKiller11 <a19baylopmej@inspedralbes.cat>
2020-12-10 13:27:00 +0100

    landing page

---
+++ php/footer.php
@@ -0,0 +1,28 @@
+<!doctype HTML>
+<html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+<div class="footer1">
+    
+    <div class="integrantes">
+        <h4> GRUP 8 </h4>
+        <p> 
+        Bayron López <br> 
+        Enrique Rillo <br> 
+        Joel Rodriguez <br> 
+        Pedro Nieto
+        </p>   
+       
+    </div>
+    <div class="botonadmin">
+        <input type="button" value="ADMIN">
+
+    </div>
+
+</div>
+       
+</body>       
+        
+</html>
