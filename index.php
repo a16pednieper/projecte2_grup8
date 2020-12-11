8ab4f99319553ddace6501b4041207789b208edb
Pedrito <a16pednieper@inspedralbes.cat>
2020-12-11 09:25:56 +0100

    headeer_footer_pedro

--- index.php
+++ index.php
@@ -5,27 +5,59 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PROJECTO 8</title>
     <link rel="stylesheet" href="css/estilo.css">
-
 </head>
+
+
+
 <body>
     
-    <?php include 'php/header.php'; ?>
+    <header class="header1">  
+    
+        <a href="#index.php"><div class="logo"><img src="img/logo1.png" width="160px"></div></a>
+        <div class="titulo"><p>GRUP 8: VIATGES</p></div>
+        <div class="botonesheader"> 
+            <input type="button" value="LOGIN">
+            <input type="button" value="REGISTRAR">
+        </div>    
+    
+    </header> 
 
     
     <?php 
-        $json = file_get_contents('prova.json');
+        $data = file_get_contents('prova.json');
+        $viajes = json_decode($data, true);
 
-        $data = json_decode($json);
+        foreach ($viajes as $viaje) {
+            echo '<pre>';
+            print_r($viaje);
+            echo '</pre>';
 
+        }
        
-        print($json);
+       
     
     ?>
 
+        <footer>
+            <div class="integrantes">
+        <h4> GRUP 8 </h4>
+        <p> 
+        Bayron López <br> 
+        Enrique Rillo <br> 
+        Joel Rodriguez <br> 
+        Pedro Nieto
+        </p>   
+       
+    </div>
+    <div class="botonadmin">
+        <input type="button" value="ADMIN">
+
+    </div>
+        </footer>
     
 
 
-    <?php include 'php/footer.php'; ?>
+    
 
 </body>
 </html>
