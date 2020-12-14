23ff344f11c16fe42678912f11a7ecc83e91f887
a14joerodnad <73338094+a14joerodnad@users.noreply.github.com>
2020-12-14 11:17:55 +0100

    bootstrap

--- index.php
+++ index.php
@@ -3,21 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PROYECTO 8</title>
+    <title>PROJECTE 8</title>
     <link rel="stylesheet" href="css/estilo.css">
+    <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+    <link href="css/styles.css" rel="stylesheet" />
 </head>
 
 <body>
     
     <header class="header1">  
-    
-        <a href="#index.php"><div class="logo"><img src="img/logo1.png" width="160px"></div></a>
-        <div class="titulo"><p>GRUP 8: VIATGES</p></div>
-        <div class="botonesheader"> 
-            <input type="button" value="LOGIN">
-            <input type="button" value="REGISTRAR">
-        </div>    
-    
+        <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+            <div class="container">
+                <a href="#index.php"><div class="logo"><img src="img/logo1.png" width="160px"></div></a>
+                <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item mx-0 mx-lg-1"><input type="button" value="LOGIN"></li>
+                        <li class="nav-item mx-0 mx-lg-1"><input type="button" value="REGISTRAR"></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>  
     </header> 
 
 
@@ -70,23 +81,48 @@
         <div class="principal">
             <h1>Experiencies</h1>
         </div>
-        <footer class="footer1">
-            <div class="integrantes">
-                <h4> GRUP 8 </h4>
-                    <p> 
-                        Bayron López <br> 
-                        Enrique Rillo <br> 
-                        Joel Rodriguez <br> 
-                        Pedro Nieto
-                    </p>          
-            </div>
 
-            <div class="botonadmin">
-                <input type="button" value="ADMIN">
-            </div>
+        <footer class="footer text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Location</h4>
+                        <p class="lead mb-0">
+                            Institut Pedralbes
+                            <br />
+                            Barcelona
+                        </p>
+                    </div>
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Around the Web</h4>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
+                    </div>
+                    <div class="col-lg-4">
+                        <h4 class="text-uppercase mb-4">Membres del Grup 8</h4>
+                        <p class="lead mb-0">
+                            Pedro Nieto, Joel Rodríguez, Enrique Rillo i Bayron Lopez
+                        </p>
+                    </div>
+                </div>
+            </div>            
         </footer>
+        
+        <!-- Copyright Section-->
+        <div class="copyright py-4 text-center text-white">
+        <div class="container"><small>Copyright © Willyrex 2020</small></div>
+        </div>
+        <div class="scroll-to-top d-lg-none position-fixed">
+        <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top"><i class="fa fa-chevron-up"></i></a>
+        </div>
     
-
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
 
     
 
