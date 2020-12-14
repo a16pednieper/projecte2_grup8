25e9161b869ef2ba4719356deb80f941607a2831
a14joerodnad <73338094+a14joerodnad@users.noreply.github.com>
2020-12-14 11:52:35 +0100

    boton admin

--- index.php
+++ index.php
@@ -94,18 +94,14 @@
                             Barcelona
                         </p>
                     </div>
-                    <div class="col-lg-4 mb-5 mb-lg-0">
-                        <h4 class="text-uppercase mb-4">Xarxes Socials</h4>
-                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
-                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
-                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
-                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
-                    </div>
                     <div class="col-lg-4">
                         <h4 class="text-uppercase mb-4">Membres del Grup 8</h4>
                         <p class="lead mb-0">
                             Pedro Nieto, Joel Rodríguez, Enrique Rillo i Bayron Lopez
                         </p>
+                    </div>
+                    <div class="col-lg-4">
+                        <input type="button" value="ADMIN">
                     </div>
                 </div>
             </div>            
