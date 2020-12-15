f6af25e499fd05fc765dd245cf4d70bb48f7caa6
ShadowKiller11 <a19baylopmej@inspedralbes.cat>
2020-12-15 13:24:39 +0100

    css

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
     <link href="css/styles.css" rel="stylesheet" />
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    
 
 
 </head>
