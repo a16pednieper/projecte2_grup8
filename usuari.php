528044e5e9ff9eb947f3926b7df7460c35360e95
Pedro <a16pednieper@inspedralbes.cat>
2020-12-16 21:57:12 +0100

    la buenaç

--- usuari.php
+++ usuari.php
@@ -9,7 +9,7 @@
         private $password;      
 
         function __construct () {
-            $this->dbname = "a16pednieper_v2";
+            $this->db_name = "a16pednieper_v2";
         }
 
         function __toString() {
