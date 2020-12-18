db26158edeb91b9fa5452d23f5544f479821d82b
Pedro <a16pednieper@inspedralbes.cat>
2020-12-18 09:35:58 +0100

    afegir experiencia php

---
+++ afegirExperiencia.php
@@ -0,0 +1,29 @@
+
+
+    <div class="afegir-exp">
+    
+        <div class="header-afegir">
+            <h4>Afegir Experiencia</h4>
+            <button type="button">X</button>       
+        </div>
+
+        <div class="body-afegir">
+            <form class="form-experiencia"></form>
+            <input id="input-titol" type="text" required="" autofocus="" placeholder="Titol Experiencia">
+            <br>
+            <input type="date" id="input-data" required="" autofocus="" placeholder="Data">        
+            <br>    
+            <textarea type="text" rows="4" id="inputText" class="form-control" placeholder="Text" required=""></textarea>
+            <br>
+            <select name="" id="">
+            <option value="1">Aventures</option>
+            <option value="2">Muntanya</option>
+            <option value="3">Familiar</option>                                   
+            </select>
+            <button type="button" id="afegirExp" class="">Afegir Experiencia</button>
+        </div>
+    
+    
+    </div>
+
+
