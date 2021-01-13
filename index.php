b645a64d0a805a58069bf4f18e8b57a681b8daac
a14joerodnad <a14joerodnad@inspedralbes.cat>
2021-01-13 12:08:23 +0100

    json y css

--- index.php
+++ index.php
@@ -18,7 +18,40 @@
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"> </script>
     <link rel="icon" type="image/vnd.microsoft.icon" href="img/favicon.ico">
 
-    <script>                      
+</head>
+
+<body>
+
+    <?php include 'header.php'; ?>
+
+    <div class="modal fade" id="miModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h4 class="modal-title" id="myModalLabel">Registro Usuario</h4>
+                </div>
+                <div class="modal-body">
+                    <?php
+                         include 'crearUsuari.php';
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="principal">
+        <div class="cabecera">
+            <h1>Experiencies</h1>
+            <img src="img/avion.gif" alt="avion" width="150" class="avion">
+        </div>
+            
+        <div class="experiences" id="experiencies">     
+
+        <script>                      
 
             var datos = [];
 
@@ -96,47 +129,7 @@
             
             </script>
 
-
-</head>
-
-<body>
-
-    <?php include 'header.php'; ?>
-
-    <div class="modal fade" id="miModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    <h4 class="modal-title" id="myModalLabel">Registro Usuario</h4>
-                </div>
-                <div class="modal-body">
-                    <?php
-                         include 'crearUsuari.php';
-                    ?>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <div class="principal">
-        <div class="cabecera">
-            <h1>Experiencies</h1>
-            <img src="img/avion.gif" alt="avion" width="150" class="avion">
-        </div>
-
-            
-        <div class="experiences" id="experiencies">     
-
-        </div>      
-
-            
-                      
-
-        
+        </div>  
 
     </div>
 
