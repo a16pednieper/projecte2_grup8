7c1468690d112c5be980c784b1e3446c79cd9261
ShadowKiller11 <a19baylopmej@inspedralbes.cat>
2021-01-14 13:35:21 +0100

    login

--- index.php
+++ index.php
@@ -42,6 +42,23 @@
         </div>
     </div>
 
+    <div class="modal fade" id="miModalo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h4 class="modal-title" id="myModalLabel">Logear Usuario</h4>
+                </div>
+                <div class="modal-body">
+                    <?php
+                         include 'logearUsuari.php';
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <div class="principal">
         <div class="cabecera">
